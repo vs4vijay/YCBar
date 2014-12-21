68393c74b80352d7b9eb5f662ea2372a31c1d432
Vijay Soni <vs4vijay@gmail.com>
2014-12-21 22:05:54 +0530

    initial setup

--- YCBar/AppDelegate.swift
+++ YCBar/AppDelegate.swift
@@ -12,16 +12,21 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var ycStatusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "Y"
+        statusItem.menu = ycStatusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    @IBAction func onShowClick(sender: NSMenuItem) {
+    }
 
 }
 
