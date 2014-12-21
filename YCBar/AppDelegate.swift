605d54d5eaa95bf2db342e88d5fac51420b1571f
Vijay Soni <vs4vijay@gmail.com>
2014-12-21 21:30:44 +0530

    Initial Commit

---
+++ YCBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  YCBar
+//
+//  Created by Vizay on 12/21/14.
+//  Copyright (c) 2014 Vizay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
